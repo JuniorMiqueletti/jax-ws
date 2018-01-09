f06e051b66f52eb3c56c8d706608c23221ca02d5
Junior Miqueletti <junior.miqueletti@matera.com>
2018-01-09 19:06:55 -0200

    adding WebMethod name and WebResult name

--- src/main/java/com/juniormiqueletti/jaxwslab/ws/StockWS.java
+++ src/main/java/com/juniormiqueletti/jaxwslab/ws/StockWS.java
@@ -2,6 +2,8 @@ package com.juniormiqueletti.jaxwslab.ws;
 
 import java.util.List;
 
+import javax.jws.WebMethod;
+import javax.jws.WebResult;
 import javax.jws.WebService;
 
 import com.juniormiqueletti.jaxwslab.dao.ItemDAO;
@@ -12,7 +14,10 @@ public class StockWS {
 
 	private ItemDAO dao = new ItemDAO();
 	
+	@WebMethod(operationName = "allItens")
+	@WebResult(name="item")
 	public List<Item> findAll(){
+		System.out.println("findAll");
 		List<Item> itens = dao.findAll();
 		return itens;
 	}
